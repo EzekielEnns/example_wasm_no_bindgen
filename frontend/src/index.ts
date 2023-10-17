4655f8f619cf3dc0b4fde3e43f2929822e9b0556
Ezekiel Enns <Ezekiel.Enns@protonmail.com>
2023-10-17 17:15:02 -0300

    finally got everything working, but tests are a bit janky

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -1,17 +1,26 @@
 //must be kept in sync with lib.rs
 interface Wasm {
     add(a:number,b:number): number;
+    plyMove(l:number,d:number,u:number,r:number):number;
+    tick():number;
+    test(): number;
+    memory: WebAssembly.Memory
+
 }
 
 fetch("./build/wasm32-unknown-unknown/debug/logic.wasm ")
   .then((res) => res.arrayBuffer())
   .then((bytees) =>
-    WebAssembly.instantiate(bytees, { })
+    WebAssembly.instantiate(bytees, {})
   )
   .then((model) => {
+      //Read:: https://radu-matei.com/blog/practical-guide-to-wasm-memory/
       const test:Wasm = (model.instance.exports as any);
       //main code here
-      alert(`${test.add(1,2)}`)
+      let ptr = test.tick();
+      console.log(model.instance.exports)
+      const arr = new Uint8Array(test.memory.buffer,ptr, 10*10)
+      console.log(arr)
       // while (true) {
       //   //wasm tick
       //   //canvas render
--- logic/src/lib.rs
+++ logic/src/lib.rs
@@ -1,10 +1,69 @@
 #![allow(dead_code)]
 
+use std::println;
+
+const WIDTH:usize = 10;
+const HEIGHT:usize = 10;
+
+static mut WORLD: Game = Game{
+   player:Entity{
+       pos: Pos{y:HEIGHT as u8/2,x:WIDTH as u8/2},
+       value: b'@'
+   },
+   level: [b'.';HEIGHT*WIDTH]
+};
+
+
 //https://tung.github.io/posts/rust-and-webassembly-without-a-bundler/
 #[no_mangle]
 pub extern "C" fn add(left: usize, right: usize) -> usize {
     left + right
 }
+#[no_mangle]                    //TODO use a single u8
+pub unsafe extern "C" fn plyMove(l:u8,d:u8,u:u8,r:u8) -> u8{
+    WORLD.player.pos.y += d;
+    WORLD.player.pos.y -= u;
+    WORLD.player.pos.x += r;
+    WORLD.player.pos.x -= l;
+    WORLD.player.pos.y
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn test()->usize{
+    println!("{}",WORLD.level.len());
+    Game::get_index(&WORLD.player.pos)
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn tick()->*const u8{
+    WORLD.level[Game::get_index(&WORLD.player.pos)] = WORLD.player.value;
+    WORLD.level.as_ptr()
+}
+
+
+#[repr(C)] 
+pub struct Game {
+    player: Entity,
+    level: [u8; WIDTH*HEIGHT]
+}
+
+impl Game {
+    fn get_index(p: &Pos)-> usize{
+        return (p.y * WIDTH as u8 + p.x) as usize;
+    }
+}
+
+#[derive(Copy,Clone,Debug)]
+pub struct Pos {
+    y:u8,
+    x:u8
+}
+
+pub struct Entity {
+    pos: Pos,
+    value: u8
+}
+
 
 #[cfg(test)]
 mod tests {
@@ -12,7 +71,10 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        unsafe {
+            println!("hi");
+            test();
+            tick();
+        }
     }
 }
